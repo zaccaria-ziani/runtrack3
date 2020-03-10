e1cf60060050dce86de2f47bb7b9b3b25a11450d
Zaccaria Ziani <Zaccaria Ziani>
2020-03-10 10:04:05 +0100

    job01/index.php job01/script.js Done

---
+++ jour02/job01/index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type = "text/Javascript" src="script.js"></script>
+</head>
+<body>
+    <article id="citation">
+        
+        "La vie a beaucoup plus d’imagination que nous".
+        
+    </article>
+
+    <button onclick="citation()">Check</button>
+    
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,5 @@
+function citation()
+{
+    var content = document.getElementById('citation').textContent;
+    console.log(content);
+}
